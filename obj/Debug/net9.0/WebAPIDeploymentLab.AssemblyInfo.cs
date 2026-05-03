3a3029b6579c49f9dc8fbd17965f8cc635f0ada8
Millions <millionkuflom@gmail.com>
2026-05-03 16:28:52 +0200

    fixing

--- obj/Debug/net9.0/WebAPIDeploymentLab.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebAPIDeploymentLab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPIDeploymentLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ffe4bf0ef8304ca0be318e0d7ae566f2a50933")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPIDeploymentLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPIDeploymentLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
